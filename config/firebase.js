3c5a01ff78a6910d5081e461f787664694aba877
ritesh-kumar-maurya <ritesh.maurya@deboxglobal.com>
2026-06-20 23:34:22 +0530

    fix issue

--- config/firebase.js
+++ config/firebase.js
@@ -1,11 +1,11 @@
-const admin = require('firebase-admin');
+// const admin = require('firebase-admin');
 
-// Parse service account key from environment variable
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+// // Parse service account key from environment variable
+// const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+// });
 
-module.exports = admin.storage().bucket();
+// module.exports = admin.storage().bucket();
